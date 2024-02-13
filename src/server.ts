7b080f244e76b701498c1b588dbd9e6edc0acce5
lucasoliveiraa <lucasjackcosta18@gmail.com>
2024-02-13 14:15:11 -0300

    :tada: Initial project

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'hel'
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
